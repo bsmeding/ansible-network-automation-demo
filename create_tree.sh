448b30fa4d3f4d818eedfaa55a0b0f91e6028df3
bsmeding <bsmeding@ansible-demo.networklab.local>
2017-10-06 23:21:16 +0200

    First stage

---
+++ create_tree.sh
@@ -0,0 +1,13 @@
+mkdir -p ./group_vars
+mkdir -p ./host_vars
+touch ./group_vars/all
+touch ./inventory
+mkdir -p ./roles/common/defaults
+mkdir -p ./roles/common/tasks
+mkdir -p ./roles/common/handlers
+mkdir -p ./roles/common/templates
+mkdir -p ./roles/common/files
+mkdir -p ./roles/common/vars
+mkdir -p ./roles/common/meta
+touch ./roles/common/tasks/main.yml
+touch ./roles/common/vars/main.yml
